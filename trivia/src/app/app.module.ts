1daa8a9ea0c7dd2f512a6f3d6cc2b30699943c25
David <31216720+Agnarr43@users.noreply.github.com>
2020-12-11 09:55:53 -0600

    beginnings of a dataservice

--- trivia/src/app/app.module.ts
+++ trivia/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginScreenComponent } from './components/login-screen/login-screen.component';
@@ -16,7 +16,8 @@ import { StatsScreenComponent } from './components/stats-screen/stats-screen.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
